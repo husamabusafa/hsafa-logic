05ee6356d94614b2e3b976fdedbc261792625ec6
husamabusafa <husam.ihab.abusafa@gmail.com>
2026-02-07 22:04:15 +0300

    Remove node_modules symlinks from version control in gateway and Next.js test app

--- react-sdk/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './types.js';
-export * from './client.js';
-export * from './hooks.js';
-export * from './utils.js';
-//# sourceMappingURL=index.d.ts.map
--- react-sdk/dist/utils.d.ts
+++
@@ -1,4 +0,0 @@
-import type { SmartSpaceMessageRecord, SmartSpaceStreamMessage } from './types.js';
-export declare function smartSpaceMessageToText(m: SmartSpaceMessageRecord): string;
-export declare function smartSpaceStreamPartsToText(parts: SmartSpaceStreamMessage['parts']): string;
-//# sourceMappingURL=utils.d.ts.map
